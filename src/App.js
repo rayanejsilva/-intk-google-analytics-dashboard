0f0f8539952ca9c98af8bce4041af1ef8c64c3d2
rayanejsilva <rayanej.silva@gmail.com>
2022-07-05 13:50:14 +0200

    logout button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import { useEffect} from 'react'
+import { useEffect, useState} from 'react'
 import './styles/main.css';
+import jwt_decode from "jwt-decode";
 
 function App() {
+  /* store user */
+  const [user, setUser] = useState({});
   const handleCredentialResponse = (response) => {
     console.log("Encoded JWT ID token: " + response.credential);
+    let userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("btn__signIn").hidden = true;
+  }
+  const handleSignOut = (event) => {
+    setUser({});
+    document.getElementById("btn__signIn").hidden = false;
+
+
   }
   useEffect(() => {
     /* global google */
@@ -20,6 +33,14 @@ function App() {
   return (
     <div className="App">
       <div id='btn__signIn'></div>
+      {Object.keys(user).length !== 0 && <button onClick={(e) => handleSignOut(e)}>Sign Out</button>}
+      
+      {user &&
+        <div>
+          <img src={user.picture} alt="user profile"></img>
+          <h3>{user.name}</h3> 
+        </div>
+      }
     </div>
   );
 }
